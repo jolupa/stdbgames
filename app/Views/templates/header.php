aa9b0c65e143a076f4b4e4f1bc3c74f22e0abeae
jolupa <jlpavon@me.com>
2020-03-15 01:10:48 +0100

    the menu on mobile works!

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -10,6 +10,7 @@
     <?php endif; ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
   <body>
     <section class="container">
