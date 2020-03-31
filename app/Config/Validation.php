8e1a32025898b9080660c18117f4d387478c7c14
jolupa <jlpavon@me.com>
2020-03-31 13:40:38 +0200

    Commit to change

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -28,6 +28,7 @@ class Validation
 	public $templates = [
 		'list'   => 'CodeIgniter\Validation\Views\list',
 		'single' => 'CodeIgniter\Validation\Views\single',
+		'my_list'=>	'error_list',
 	];
 
 	//--------------------------------------------------------------------
