86b874ff7f03cf088abf845f27d8633a58b1238f
jolupa <jlpavon@me.com>
2020-04-11 03:57:16 +0200

    correct vote for well

--- app/Views/users/interaction.php
+++ app/Views/users/interaction.php
@@ -6,7 +6,7 @@
       <?php endif; ?>
       <?php if ($vote == 0 & $item['gRelease'] <= date('Y-m-d')): ?>
         <a title="VOTE GREAT" href="<?= base_url() ?>/users/uservote/upvote/<?= $item['gId'] ?>/<?= session('id') ?>"><span class="tag is-primary is-medium"><span class="heading">GREAT</span></span></a>
-        <a title="VOTE WELL..." href="<?= base_url() ?>/users/uservote/upvote/<?= $item['gId'] ?>/<?= session('id') ?>"><span class="tag is-warning is-medium"><span class="heading">WELL...</span></span></a>
+        <a title="VOTE WELL..." href="<?= base_url() ?>/users/uservote/well/<?= $item['gId'] ?>/<?= session('id') ?>"><span class="tag is-warning is-medium"><span class="heading">WELL...</span></span></a>
         <a title="VOTE WHAT THE..." href="<?= base_url() ?>/users/uservote/downvote/<?= $item['gId'] ?>/<?= session('id') ?>"><span class="tag is-danger is-medium"><span class="heading">WHAT THE...</span></span></a>
       <?php endif; ?>
     </p>
