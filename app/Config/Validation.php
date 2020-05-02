5b1b469c26b75c32d69a3f7c192de0ae9fd58f8a
jolupa <jlpavon@me.com>
2020-05-02 17:07:38 +0200

    new CodeIgniter version

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -28,7 +28,6 @@ class Validation
 	public $templates = [
 		'list'   => 'CodeIgniter\Validation\Views\list',
 		'single' => 'CodeIgniter\Validation\Views\single',
-		'my_list' => '_errors_list',
 	];
 
 	//--------------------------------------------------------------------
