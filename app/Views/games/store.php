5691b819737434b51b141c341ef7a627e1fd60c0
jolupa <jlpavon@me.com>
2020-05-16 00:33:19 +0200

    changed size

--- app/Views/games/store.php
+++ app/Views/games/store.php
@@ -1,1 +1,1 @@
-<a href="https://stadia.google.com/store/details/<?= $game['gAppid'] ?>/sku/<?= $game['gSku'] ?>" target="_blank" title="Go to <?= $game['gName'] ?>"><button class="button is-danger is-medium has-text-white">Go to Stadia Store</button></a>
+<a href="https://stadia.google.com/store/details/<?= $game['gAppid'] ?>/sku/<?= $game['gSku'] ?>" target="_blank" title="Go to <?= $game['gName'] ?>"><button class="button is-danger is-small has-text-white">Go to Stadia Store</button></a>
