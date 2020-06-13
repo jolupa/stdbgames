ce98c4be6f67cfd2d200486eba9008864cb0c950
jolupa <jlpavon@me.com>
2020-06-13 12:52:48 +0200

    corrected some typos

--- app/Views/templates/about.php
+++ app/Views/templates/about.php
@@ -3,49 +3,43 @@
     <div class="columns">
       <div class="column is-full-width">
         <p class="title is-3">What is this!</p>
-        <p class="subtitle is-5">This is only a spare time project to retake the PHP coding I left behind for about 25 years or more.<br>
+        <p class="subtitle">This is only a spare time project to retake the PHP coding I left behind for about 25 years or more.<br>
         The project is made with <strong><a href="https://codeigniter.com" target="_blank">CodeIgniter 4</a></strong> and with <strong><a href="https://bulma.io" target="_blank">BulmaCSS</a></strong>, I love both projects.<br>
         Hope you enjoy, and if you have some idea to implement just send me and email to <strong>hello at dot stdb dot gam[es]</strong></p>
-    		<p>&nbsp;</p>
     		<p class="title is-3">Privacy Policy of Stadia GamesDB!</p>
-    		<p class="subtitle is-5">Stadia GamesDB! operates the https://stdb.games website, which provides the SERVICE.<br>
+    		<p class="subtitle">Stadia GamesDB! operates the https://stdb.games website, which provides the SERVICE.<br>
     			This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Stadia Games DB! website.<br>
     			If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.<br>
     			The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at <a href="<?= base_url() ?>">https://stdb.games</a>, unless otherwise defined in this Privacy Policy. Our Privacy Policy was created with the help of the Privacy Policy Template and the Disclaimer Generator.</p>
-    		<p>&nbsp;</p>
     		<p class="title is-3">Information Collection and Use</p>
-    		<p class="subtitle is-5">For a better experience while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and postal address. The information that we collect will be used to contact or identify you.<br>
-    		<span class="title is-3">Log Data</span><br>
-    		We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computerâ€™s Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.<br>
-    		<span class="title is-3">Cookies</span><br>
-    		Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computerâ€™s hard drive.<br>
-    		Our website uses these "cookies" to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service.<br>
-    		For more general information on cookies, please read "What Are Cookies".</p>
-    		<p>&nbsp;</p>
+    		<p class="subtitle">For a better experience while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and postal address. The information that we collect will be used to contact or identify you.</p>
+        <div class="content">
+      		<ul>
+            <li>Log Data: We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.</li>
+            <li>Cookies: Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computers hard drive. Our website uses these "cookies" to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service. For more general information on cookies, please read "What Are Cookies".</li>
+          </ul>
+        </div>
     		<p class="title is-3">Service Providers</p>
-    		<p class="subtitle is-5">We may employ third-party companies and individuals due to the following reasons:<br>
-    		<ul>
-    			<li>To facilitate our Service;</li>
-    			<li>To provide the Service on our behalf;</li>
-    			<li>To perform Service-related services; or</li>
-    			<li>To assist us in analyzing how our Service is used.</li>
-    		</ul>
-    		<br>We want to inform our Service users that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
-    		<p>&nbsp;</p>
+    		<p class="subtitle">We may employ third-party companies and individuals due to the following reasons:</p>
+        <div class="content">
+      		<ul>
+      			<li>To facilitate our Service;</li>
+      			<li>To provide the Service on our behalf;</li>
+      			<li>To perform Service-related services; or</li>
+      			<li>To assist us in analyzing how our Service is used.</li>
+      		</ul>
+        </div>
+    		<p class="subtitle">We want to inform our Service users that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
     		<p class="title is-3">Security</p>
-    		<p class="subtitle is-5">We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
-    		<p>&nbsp;</p>
+    		<p class="subtitle">We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
     		<p class="title is-3">Links to Other Sites</p>
-    		<p class="subtitle is-5">Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
-    		<p>&nbsp;</p>
+    		<p class="subtitle">Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
     		<p class="title is-3">Children's Privacy</p>
-    		<p class="subtitle is-5">Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
-    		<p>&nbsp;</p>
+    		<p class="subtitle">Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
     		<p class="title is-3">Changes to This Privacy Policy</p>
-    		<p class="subtitle is-5">We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.</p>
-    		<p>&nbsp;</p>
+    		<p class="subtitle">We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.</p>
     		<p class="title is-3">Contact Us</p>
-    		<p class="subtitle is-5">If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
+    		<p class="subtitle">If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
       </div>
     </div>
 	</div>
