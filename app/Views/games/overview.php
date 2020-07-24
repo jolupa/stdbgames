db5707f2452c3b2bf3aa91accc7ac99a1cdfe3cd
jolupa <jlpavon@me.com>
2020-07-24 21:35:58 +0200

    Fixed the wishlist button for games not released

--- app/Views/games/overview.php
+++ app/Views/games/overview.php
@@ -79,7 +79,7 @@
       <?php if($game['pro_from'] && $game['pro'] == 0): ?>
         <button class="button is-danger has-text-white is-small mt-1 mr-2">Was free from&nbsp;<strong><?= $game['pro_from'] ?></strong>&nbsp;until&nbsp;<strong><?= $game['pro_till'] ?></strong></button>
       <?php endif; ?>
-      <?php if(date('Y-m-d') >= $game['release'] && session('logged') == true): ?>
+      <?php if(session('logged') == true): ?>
         <?= view_cell('App\Controllers\Libraries::isinlibrary', 'id='.$game['id']) ?>
       <?php endif; ?>
     </p>
--- app/Views/libraries/librarybutton.php
+++ app/Views/libraries/librarybutton.php
@@ -1,7 +1,9 @@
-<?php if($library == false): ?>
-  <a href="<?= base_url() ?>/add/tolibrary/<?= $game['id'] ?>"><button class="button is-danger has-text-white is-small mt-1 mr-2">Add To Library</button></a>
-<?php else: ?>
-  <button class="button is-primary has-text-dark is-small mt-1 mr-2">In Your Library</button>
+<?php if(date('Y-m-d') >= $game['release']): ?>
+  <?php if($library == false): ?>
+    <a href="<?= base_url() ?>/add/tolibrary/<?= $game['id'] ?>"><button class="button is-danger has-text-white is-small mt-1 mr-2">Add To Library</button></a>
+  <?php else: ?>
+    <button class="button is-primary has-text-dark is-small mt-1 mr-2">In Your Library</button>
+  <?php endif; ?>
 <?php endif; ?>
 <?php if($library == false): ?>
   <?= view_cell('App\Controllers\Wishlists::isinwishlist', 'id='.$game['id']) ?>
