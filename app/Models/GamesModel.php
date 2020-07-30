32641cf7a82705f6c37cfb12357853cece0b26df
jolupa <jlpavon@me.com>
2020-07-30 02:25:19 +0200

    fixed the typo typo

--- app/Models/GamesModel.php
+++ app/Models/GamesModel.php
@@ -182,6 +182,7 @@
                                 games.slug,
                                 games.image,
                                 games.release,
+                                game.pro_from,
                                 developers.name AS developer_name,
                                 publishers.name AS publisher_name')
                       ->join('developers', 'developers.id = games.developer_id')
@@ -195,7 +196,6 @@
                                 games.slug,
                                 games.image,
                                 games.release,
-                                games.pro_from,
                                 developers.name AS developer_name,
                                 publishers.name AS publisher_name')
                       ->join('developers', 'developers.id = games.developer_id')
