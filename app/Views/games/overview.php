017b7fc9e32a779274d5482120e40ff90156649c
jolupa <jlpavon@me.com>
2020-09-21 19:05:59 +0200

    Now the games that will be PRO on future are labeled correctly

--- app/Views/games/overview.php
+++ app/Views/games/overview.php
@@ -42,6 +42,9 @@
                   <a class="button has-text-white is-small mt-1 mr-2 is-borderless" style="background-color: #FC4A1F; border: none;" href="https://stadia.google.com/player/<?= $game['appid'] ?>" target="_blank">Play on Stadia</a>
                 <?php endif; ?>
               <?php endif; ?>
+              <?php if($game['pro'] == 1 && $game['pro_from' === '']): ?>
+                <button class="button is-primary has-text-dark is-small mt-1 mr-2">Will be&nbsp;<strong>Free</strong>&nbsp;on Launch</button>
+              <?php endif; ?>
               <?php if($game['pro'] == 1 && date('Y-m-d') > $game['pro_from'] && $game['release'] !== 'TBA'): ?>
                 <button class="button is-primary has-text-dark is-small mt-1 mr-2">Free for Pro&nbsp;<strong>Now!</strong></button>
               <?php endif; ?>
