69b40422a807f392be430789ab4d9ab5bf89f927
jolupa <jlpavon@me.com>
2020-10-10 18:45:05 +0200

    Added comment to Controller

--- app/Controllers/Games.php
+++ app/Controllers/Games.php
@@ -6,6 +6,12 @@
   helper(['text']);
 
   class Games extends Controller{
+    //Index function
+    //getProGames List all Pro Games
+    //getSoonGames all the games coming
+    //getLastsGames the last games added to DB
+    //getLastsUpdatedGames the last games updated on DB
+    //getMonthRelease the games releasing the actual month
     public function index(){
       $gamemodel = new GamesModel();
       $data['pro'] = $gamemodel->getProGames();
@@ -17,11 +23,15 @@
       echo view('games/index', $data);
       echo view('templates/footer');
     }
+    /*
+    //getMonthRelease list the games releasing the same day the game we have in overview
     public function getmonthrelease(){
       $gamemodel = new GamesModel();
       $data['month'] = $gamemodel->getMonthRelease();
       return view('games/thismonth', $data);
     }
+    */
+    //overview is the main game view page all the game information
     public function overview($slug, $wrong = false){
       $gamemodel = new GamesModel();
       $data['game'] = $gamemodel->gameOverview($slug);
@@ -31,6 +41,15 @@
       echo view('games/overview', $data);
       echo view('templates/footer', $data);
     }
+    //list all the games and we can filter by:
+    //all -> All games
+    //soon -> Games coming
+    //launched -> Games already on Store
+    //firstonstadia -> Games with that feature
+    //stadiaexclusive -> Exclusives for the platform
+    //earlyaccess -> Games on early access
+    //pro -> All the Games that hit the Pro
+    //rumors -> Games not confirmed for the platform
     public function list($type = 'all'){
       $gamemodel = new GamesModel();
       $data['list'] = $gamemodel->listAllGames($type);
@@ -39,11 +58,13 @@
       echo view('games/list', $data);
       echo view('templates/footer');
     }
+    //Games launched the same day that the game we are watching
     public function releasebydate($id, $date){
       $gamemodel = new GamesModel();
       $data['released_day'] = $gamemodel->releaseByDate($id, $date);
       return view('games/releasebydate', $data);
     }
+    //Form for adding new games
     public function insertform(){
       if(session('role') == 1){
         $data['editor'] = true;
@@ -57,6 +78,8 @@
         echo view('templates/footer');
       }
     }
+    //Insert the game on DB!
+    //Verufications: Name -> not empty and not on DB!
     public function creategamedb(){
       $gamemodel = new GamesModel();
       $session = \Config\Services::session();
@@ -115,6 +138,8 @@
                         ->save(ROOTPATH.'public/images/'.$newname.'-thumb.jpeg');
           unlink(WRITEPATH.'uploads/'.$newname);
         }
+        $gamemodel->createGameDb($data);
+        //If the game is inserted correctly we send a Tweet
         require(ROOTPATH.'twitter.php');
         if($data['rumor'] == 1){
           $statusmessage = "RUMOR!! Game Added to DB! ".$data['name']." https://stdb.games/game/".$data['slug'];
@@ -123,10 +148,10 @@
         }
         $connection = new TwitterOAuth($consumerkey, $consumersecret, $token, $tokensecret);
         $connection->post("statuses/update", ["status" => $statusmessage]);
-        $gamemodel->createGameDb($data);
         return redirect()->to('/game/'.$data['slug']);
       }
     }
+    //Form to update the game on DB!
     public function updateform($slug){
       if(session('role') == 1){
         $gamemodel = new GamesModel();
@@ -142,6 +167,7 @@
         echo view('templates/footer');
       }
     }
+    //Updating the game on DB!
     public function updategamedb(){
       $gamemodel = new GamesModel();
       $data['id'] = $this->request->getVar('id');
@@ -160,6 +186,7 @@
       $data['developer_id'] = $this->request->getVar('developer_id');
       $data['publisher_id'] = $this->request->getVar('publisher_id');
       $data['about'] = $this->request->getVar('about');
+      $data['rumour'] = $this->request->getVar('rumour');
       if($this->request->getVar('rumor') !== null){
         $data['rumor'] = 1;
       } else {
@@ -191,27 +218,31 @@
       } else {
         $data['image'] = $this->request->getVar('oldimage');
       }
+      $connection = new TwitterOAuth($consumerkey, $consumersecret, $token, $tokensecret);
+      $connection->post("statuses/update", ["status" => $statusmessage]);
+      $gamemodel->updateGameDb($data);
+      //If we update the game correctly we send a tweet
       require(ROOTPATH.'twitter.php');
       if($data['rumor'] == 1){
         $statusmessage = "RUMOR!! Game Updated on DB! ".$data['name']." https://stdb.games/game/".$data['slug'];
       } else {
         $statusmessage = "Game Updated on DB! ".$data['name']." https://stdb.games/game/".$data['slug'];
       }
-      $connection = new TwitterOAuth($consumerkey, $consumersecret, $token, $tokensecret);
-      $connection->post("statuses/update", ["status" => $statusmessage]);
-      $gamemodel->updateGameDb($data);
       return redirect()->to('/game/'.$data['slug']);
     }
+    //About the site page
     public function about(){
       echo view('templates/header');
       echo view('templates/about');
       echo view('templates/footer');
     }
+    //A select form field for some forms
     public function allgames(){
       $gamemodel = new GamesModel();
       $data['game'] = $gamemodel->getAllGames();
       return view('games/gameselection', $data);
     }
+    //In case we want to delete some game
     public function deletegame($id){
       $gamemodel = new GamesModel();
       $data[] = $gamemodel->getGameById($id);
