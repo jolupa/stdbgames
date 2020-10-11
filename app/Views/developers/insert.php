52ffcf2fd167c1309f2da127997ecad2b9e5f928
jolupa <jlpavon@me.com>
2020-10-11 19:38:30 +0200

    updated design on publishers and developers admin pages to follow current style

--- app/Views/developers/insert.php
+++ app/Views/developers/insert.php
@@ -1,45 +1,37 @@
-<?php if(isset($error)): ?>
-  <div class="columns">
-    <div class="column has-text-centered">
-      <div class="content">
+<div class="container">
+  <section class="section">
+    <div class="content">
+      <?php if(isset($error)): ?>
         <p><?= $error ?></p>
-      </div>
+      <?php else: ?>
+        <p class="subtitle is-5">Insert new</p>
+        <p class="title is-3">Developer:</p>
+        <form action="<?= base_url() ?>/developers/createdeveloperdb" method="post" enctype="multipart/form-data">
+          <div class="field">
+            <div class="control">
+              <input type="text" class="input" name="name" placeholder="Developer's Name">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <input type="text" class="input" name="url" placeholder="Developer's Site">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <textarea name="about" class="textarea" id="textarea" placeholder="Info about the developer"></textarea>
+            </div>
+          </div>
+          <div class="field is-grouped">
+            <div class="control">
+              <button class="button is-small is-primary has-text-dark" value="submit">Add Developer!</button>
+            </div>
+            <div class="control">
+              <button class="button is-small is-danger has-text-white" value="reset">Start Over</button>
+            </div>
+          </div>
+        </form>
+      <?php endif; ?>
     </div>
-  </div>
-<?php else: ?>
-  <div class="columns">
-    <div class="column">
-      <p class="subtitle is-5">Insert new</p>
-      <p class="title is-3">Developer:</p>
-    </div>
-  </div>
-  <div class="columns">
-    <div class="column">
-      <form action="<?= base_url() ?>/developers/createdeveloperdb" method="post" enctype="multipart/form-data">
-        <div class="field">
-          <div class="control">
-            <input type="text" class="input" name="name" placeholder="Developer's Name">
-          </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <input type="text" class="input" name="url" placeholder="Developer's Site">
-          </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <textarea name="about" class="textarea" id="textarea" placeholder="Info about the developer"></textarea>
-          </div>
-        </div>
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-small is-primary has-text-dark" value="submit">Add Developer!</button>
-          </div>
-          <div class="control">
-            <button class="button is-small is-danger has-text-white" value="reset">Start Over</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-<?php endif; ?>
+  </section>
+</div>
--- app/Views/developers/updateform.php
+++ app/Views/developers/updateform.php
@@ -1,47 +1,39 @@
-<?php if(isset($error)): ?>
-  <div class="columns my-2">
-    <div class="column has-text-centered">
-      <div class="content">
+<div class="container">
+  <section class="section">
+    <div class="content">
+      <?php if(isset($error)): ?>
         <p><?= $error ?></p>
-      </div>
+      <?php else: ?>
+        <p class="subtitle is-5">Update</p>
+        <p class="title is-3">Developer</p>
+        <form action="<?= base_url() ?>/developers/updatedeveloperdb" method="post">
+          <input type="hidden" name="id" value="<?= $developer['id'] ?>">
+          <input type="hidden" name="slug" value="<?= $developer['slug'] ?>">
+          <div class="field">
+            <div class="control is-expanded">
+              <input type="text" class="input" name="name" value="<?= $developer['name'] ?>">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <input type="text" class="input" name="url" value="<?= $developer['url'] ?>">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <textarea class="textarea" id="textarea" name="about"><?= $developer['about'] ?></textarea>
+            </div>
+          </div>
+          <div class="field is-grouped is-grouped-multiline">
+            <div class="control">
+              <button class="button is-small is-primary has-text-dark" value="submit">Update Developer!</button>
+            </div>
+            <div class="control">
+              <button class="button is-small is-danger has-text-white" value="reset">Clear All!</button>
+            </div>
+          </div>
+        </form>
+      <?php endif; ?>
     </div>
-  </div>
-<?php else: ?>
-  <div class="columns mt-2">
-    <div class="column">
-      <p class="subtitle is-5">Update</p>
-      <p class="title is-3">Developer</p>
-    </div>
-  </div>
-  <div class="columns mb-2">
-    <div class="column">
-      <form action="<?= base_url() ?>/developers/updatedeveloperdb" method="post">
-        <input type="hidden" name="id" value="<?= $developer['id'] ?>">
-        <input type="hidden" name="slug" value="<?= $developer['slug'] ?>">
-        <div class="field">
-          <div class="control is-expanded">
-            <input type="text" class="input" name="name" value="<?= $developer['name'] ?>">
-          </div>
-        </div>
-        <div class="field">
-          <div class="control is-expanded">
-            <input type="text" class="input" name="url" value="<?= $developer['url'] ?>">
-          </div>
-        </div>
-        <div class="field">
-          <div class="control is-expanded">
-            <textarea class="textarea" id="textarea" name="about"><?= $developer['about'] ?></textarea>
-          </div>
-        </div>
-        <div class="field is-grouped is-grouped-multiline">
-          <div class="control">
-            <button class="button is-small is-primary has-text-dark" value="submit">Update Developer!</button>
-          </div>
-          <div class="control">
-            <button class="button is-small is-danger has-text-white" value="reset">Clear All!</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-<?php endif; ?>
+  </section>
+</div>
