13c03df6cc16059537513237cb70428cf77a3ecc
jolupa <jlpavon@me.com>
2020-10-11 14:06:29 +0200

    changed icons presented for media members users

--- app/Views/reviews/reviewfrontpage.php
+++ app/Views/reviews/reviewfrontpage.php
@@ -12,7 +12,7 @@
             <img class="image is-64x64" src="<?= base_url() ?>/images/<?= $review['game_image'] ?>-thumb.jpeg" alt="<?= $review['game_name'] ?>" title="<?= $review['game_name'] ?>">
           </figure>
           <div class="media-content">
-            <p><?= $review['user_name'] ?>&nbsp;<?php if($review['user_role'] == 2): ?><span class="has-background-danger has-text-white subtitle is-7 px-1">MEDIA MEMBER</span><?php endif; ?></p>
+            <p><?= $review['user_name'] ?>&nbsp;<?php if($review['user_role'] == 2): ?><span class="icon has-text-danger is-small"><i class="far fa-newspaper"></i></span><?php endif; ?></p>
             <p class="title is-7">
               <?php if(!empty($review['about'])): ?>
                 Reviewed <a href="<?= base_url() ?>/game/<?= $review['game_slug'] ?>#Review<?= $review['id'] ?>"><?= character_limiter($review['game_name'], 15, '...') ?></a>
