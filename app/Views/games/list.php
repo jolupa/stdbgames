78de126479052e65225216c8ffefef3fc9eae192
jolupa <jlpavon@me.com>
2020-10-12 13:16:44 +0200

    changed to new rumor icon

--- app/Views/games/list.php
+++ app/Views/games/list.php
@@ -63,7 +63,7 @@
                 </p>
               </figure>
               <div class="media-content">
-                <p class="title is-5"><?php if($list['rumor'] == 1): ?><span class="icon has-text-danger" title="RUMOR!"><i class="fas fa-exclamation-triangle"></i></span>&nbsp;<?php endif; ?><a href="<?= base_url() ?>/game/<?= $list['slug'] ?>"><?= character_limiter($list['name'], 15, '...') ?></a></p>
+                <p class="title is-5"><?php if($list['rumor'] == 1): ?><span class="icon has-text-danger" title="RUMOR!"><i class="fas fa-user-secret"></i></span>&nbsp;<?php endif; ?><a href="<?= base_url() ?>/game/<?= $list['slug'] ?>"><?= character_limiter($list['name'], 15, '...') ?></a></p>
                 <p class="subtitle is-7">Developer <?= $list['developer_name'] ?> / Publisher <?= $list['publisher_name'] ?><br>
                   <?php if($list['release'] == '2099-01-01' || $list['release'] == 'TBA'): ?>
                     TBA
