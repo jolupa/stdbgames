81e3c6e4e0ebd158d5e6b4053c26f16579b11c15
jolupa <jlpavon@me.com>
2020-10-17 21:16:17 +0200

    changed the search method on search function

--- app/Views/search/navbarsearchform.php
+++ app/Views/search/navbarsearchform.php
@@ -1,11 +1,11 @@
 <div class="content">
-  <form action="<?= base_url() ?>/search/result" method="post">
+  <form action="<?= base_url() ?>/search/result" method="get">
     <div class="field has-addons">
       <div class="control is-expanded">
         <input class="input is-small" name="keyword" type="text">
       </div>
       <div class="control">
-        <button class="button is-primary has-text-dark is-small" name="submit">Search!</button>
+        <button class="button is-primary has-text-dark is-small">Search!</button>
       </div>
     </div>
   </form>
