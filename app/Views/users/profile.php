081e21f29089830366f61d2d39dbd50aac1404fd
jolupa <jolupameister@gmail.com>
2021-03-03 12:22:17 +0100

    We don't want to know the time the user birht

--- app/Views/users/profile.php
+++ app/Views/users/profile.php
@@ -17,7 +17,7 @@
           <div class="media-content">
             <p class="title is-5"><?= $user['name'] ?> <?php if($user['role'] == 2): ?>&nbsp;<span class="has-background-danger has-text-white subtitle is-7 px-1">MEDIA MEMBER</span><?php endif; ?></p>
             <p class="subtitle is-7">E-mail: <?= $user['email'] ?><br>
-              Birthdate: <?= date('d-m-Y H:m:s', strtotime($user['birth_date'])) ?><br>
+              Birthdate: <?= date('d-m-Y', strtotime($user['birth_date'])) ?><br>
               Registered: <?= date('d-m-Y H:m:s', strtotime($user['created_at'])) ?>
             </p>
             <a href="<?= base_url() ?>/user/edit/<?= $user['id'] ?>"><p class="button is-primary is-small has-text-dark">Edit Profile</p></a>
