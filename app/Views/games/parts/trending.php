91280d9e5324042f711cdf6fe0297eabaa901605
jolupa <jolupameister@gmail.com>
2021-03-12 12:02:31 +0100

    limiting character for a good read on mobile version

--- app/Views/games/parts/trending.php
+++ app/Views/games/parts/trending.php
@@ -4,21 +4,21 @@
   <div class="columns">
     <div class="column is-7">
       <div class="content" style="background: url('<?= base_url() ?>/images/<?= $trending[0]['image'] ?>.jpeg'); size: cover; background-position: center; height: 250px;">
-        <tag class="tag title is-7 mt-2 ml-2 has-background-green-eagle"><a href="<?= base_url() ?>/game/<?= $trending[0]['slug'] ?>" title="<?= $trending[0]['name'] ?>"><?= $trending[0]['name'] ?></a></tag>
+        <tag class="tag title is-7 mt-2 ml-2 has-background-green-eagle"><a href="<?= base_url() ?>/game/<?= $trending[0]['slug'] ?>" title="<?= $trending[0]['name'] ?>"><?= character_limiter($trending[0]['name'], 15, '...') ?></a></tag>
       </div>
     </div>
     <div class="column is-5">
       <div class="columns">
         <div class="column">
           <div class="content" style="background: url('<?= base_url() ?>/images/<?= $trending[1]['image'] ?>.jpeg'); size: cover; background-position: center; height: 120px;">
-            <tag class="tag title is-7 mt-2 ml-2 has-background-green-eagle"><a href="<?= base_url() ?>/game/<?= $trending[1]['slug'] ?>" title="<?= $trending[1]['name'] ?>"><?= $trending[1]['name'] ?></a></tag>
+            <tag class="tag title is-7 mt-2 ml-2 has-background-green-eagle"><a href="<?= base_url() ?>/game/<?= $trending[1]['slug'] ?>" title="<?= $trending[1]['name'] ?>"><?= character_limiter($trending[1]['name'], 15, '...') ?></a></tag>
           </div>
         </div>
       </div>
       <div class="columns">
         <div class="column">
           <div class="content" style="background: url('<?= base_url() ?>/images/<?= $trending[2]['image'] ?>.jpeg'); size: cover; background-position: center; height: 106px;">
-            <tag class="tag title is-7 mt-2 ml-2 has-background-green-eagle"><a href="<?= base_url() ?>/game/<?= $trending[2]['slug'] ?>" title="<?= $trending[2]['name'] ?>"><?= $trending[2]['name'] ?></a></tag>
+            <tag class="tag title is-7 mt-2 ml-2 has-background-green-eagle"><a href="<?= base_url() ?>/game/<?= $trending[2]['slug'] ?>" title="<?= $trending[2]['name'] ?>"><?= character_limiter($trending[2]['name'], 15, '...') ?></a></tag>
           </div>
         </div>
       </div>
