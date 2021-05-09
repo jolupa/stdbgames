263d8926c4325e97e48689abdcc1bfe80399a721
jolupa <jolupameister@gmail.com>
2021-05-09 18:49:11 +0200

    solved error with navbar on mobile

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -28,3 +28,10 @@ $(document).ready(function(){
     $('#error').hide();
   });
 });
+
+$(document).ready(function(){
+  $('.navbar-burger').click(function(){
+    $('.navbar-burger').toggleClass('is-active');
+    $('.navbar-menu').toggleClass('is-active');
+  });
+});
