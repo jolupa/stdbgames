02c513bc696ffb3de47faba65ebe0a7f72f9861a
jolupa <jolupameister@gmail.com>
2021-05-18 10:57:43 +0200

    added buy me a coffe overlay

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -28,6 +28,8 @@
     ga('create', 'UA-179938115-1', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+  <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="stadiagamesdb" data-description="Support me on Buy me a coffee!" data-message="" data-color="#ff813f" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 </body>
 
 </html>
