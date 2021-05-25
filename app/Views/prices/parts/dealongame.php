7c95ec99c235ca004ba5a1f9e9ff40355ab4ed57
jolupa <jolupameister@gmail.com>
2021-05-25 18:56:08 +0200

    now yes!

--- app/Views/prices/parts/dealongame.php
+++ app/Views/prices/parts/dealongame.php
@@ -3,11 +3,11 @@
 <?php else: ?>
   <?php if ( isset ($dealongame['price_pro']) || isset ($dealongame['price_nonpro']) ): ?>
 
-  | <?php if ( $dealongame['price_nonpro'] != '' ): ?><strike>Pri <?= $game['price'] ?>&nbsp;€</strike><?php else: ?> Pri <?= $game['price'] ?>&nbsp;€<?php endif; ?><?php if ( $dealongame['price_pro'] != '' && $game['pro'] != 1 ): ?> | Pro <?= $dealongame['price_pro'] ?>&nbsp;€<?php elseif ( $game['pro'] == 1 && $game['pro_from'] < date('Y-m-d') ): ?> | Free for Pro<?php endif; ?><?php if ( $dealongame['price_nonpro'] != '' ): ?> | All <?= $dealongame['price_nonpro'] ?>&nbsp;€<?php endif; ?>
+  | <?php if ( $dealongame['price_nonpro'] != '' ): ?><strike>Pri <?= $game['price'] ?>&nbsp;€</strike><?php else: ?> Pri <?= $game['price'] ?>&nbsp;€<?php endif; ?><?php if ( $dealongame['price_pro'] != '' && $game['pro'] == 0 ): ?> | Pro <?= $dealongame['price_pro'] ?>&nbsp;€<?php endif; ?><?php if ( $game['pro'] == 1 && $game['pro_from'] < date('Y-m-d') ): ?> | Free for Pro<?php endif; ?><?php if ( $dealongame['price_nonpro'] != '' ): ?> | All <?= $dealongame['price_nonpro'] ?>&nbsp;€<?php endif; ?>
 
   <?php else: ?>
 
-    | Pri <?php if ( $game['price'] == '' ): ?>TBA<?php else: ?><?= $game['price'] ?> €<?php endif; ?><?php if ( $game['pro'] == 1 ): ?> | Free for Pro<?php endif; ?>
+    | Pri <?php if ( $game['price'] == '' ): ?>TBA<?php else: ?><?= $game['price'] ?> €<?php endif; ?><?php if ( $game['pro'] == 1 && $game['pro_from'] < date ('Y-m-d') ): ?> | Free for Pro<?php endif; ?>
 
   <?php endif; ?>
 
