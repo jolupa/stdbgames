27243975d475c431129be577b1acf61ee1dc9f8c
jolupa <jolupameister@gmail.com>
2021-06-03 11:42:44 +0200

    Codeigniter commit for bindings withs sqlite3 problem.

--- vendor/codeigniter4/framework/system/Database/Query.php
+++ vendor/codeigniter4/framework/system/Database/Query.php
@@ -315,13 +315,13 @@ class Query implements QueryInterface
 	 *
 	 * @return void
 	 *
-	 * @see https://regex101.com/r/EUEhay/2 Test
+	 * @see https://regex101.com/r/qvzPaZ/1
 	 */
 	protected function compileBinds()
 	{
 		$sql = $this->finalQueryString;
 
-		$hasNamedBinds = preg_match('/:(.+):/', $sql) === 1;
+		$hasNamedBinds = preg_match('/:((?!=).+):/', $sql) === 1;
 
 		if (empty($this->binds)
 			|| empty($this->bindMarker)
