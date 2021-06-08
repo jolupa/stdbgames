1cb76539254de8dd05d0e8cedde589e27ddb5977
jolupa <jolupameister@gmail.com>
2021-06-08 23:38:27 +0200

    more tweaking on editions

--- app/Views/editions/parts/editionsbygame.php
+++ app/Views/editions/parts/editionsbygame.php
@@ -35,6 +35,23 @@
               <?php endif; ?>
             </span>
           </div>
+          <div class="is-overlay is-hidden-desktop" style="top:auto; bottom:5px; left:auto; right:5px;">
+            <span class="icon-text">
+              <?php if ( session ( 'logged' ) == true && session ( 'role' ) == 1 ): ?>
+                <a href="<?= base_url ( 'editions/updateformeditions/'.$editions['id'] ) ?>"><tag class="tag is-minion"><span>Update</span></tag></a>
+              <?php endif; ?>
+              <?php if ( session ( 'likes' ) != null && in_array ( $editions['like'], session ( 'likes' ) ) ): ?>
+                <tag class="tag is-coral ml-1"><span class="icon"><i class="fas fa-thumbs-up"></i></span> <span><?= $editions['like'] ?> Likes</span></tag>
+              <?php else: ?>
+                <a href="<?= base_url( '/games/like/'.$editions['id'] ) ?>"><tag class="tag is-info ml-1"><span class="icon"><i class="fas fa-thumbs-up"></i></span> <span><?= $editions['like'] ?> Likes</span></tag></a>
+              <?php endif; ?>
+              <?php if ( session ( 'dislikes' ) != null && in_array ( $editions['dislike'], session ( 'dislikes' ) ) ): ?>
+                <tag class="tag is-coral ml-1"><span class="icon"><i class="fas fa-thumbs-down"></i></span> <span><?= $editions['dislike'] ?> Dislikes</span></tag>
+              <?php else: ?>
+                <a href="<?= base_url ( '/game/dislike/'.$editions['id'] ) ?>"><tag class="tag is-info ml-1"><span class="icon"><i class="fas fa-thumbs-down"></i></span> <span><?= $editions['dislike'] ?> Dislikes</span></tag></a>
+              <?php endif; ?>
+            </span>
+          </div>
         </div>
         <div class="card-content">
           <p class="title is-5"><?= $editions['name'] ?></p>
